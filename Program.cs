619d3e99cee7062477fb8b8e3d626cbefc5d6d29
HP <HP@DESKTOP-T6A34MC>
2024-11-27 21:46:34 +0500

    Модель представления данных. Бугрина

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace SnakeBugrina
 {
